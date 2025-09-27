73047b0a0d478cf3a4860a7087786afb9a2082a5
janmejay <janmejays236@gmail.com>
2025-09-27 14:27:30 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import Stats from './components/Stats'
 import ActivityGraph from './components/ActivityGraph'
 import Skills from './components/Skills'
 import Projects from './components/Projects'
-import QuickList from './components/QuickList'
+// import QuickList from './components/QuickList'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 import Starfield from './components/Starfield'
 import Experience from './components/Experience'
-import About from './components/About'
+// import About from './components/About'
 
 export default function App(){
   return (
